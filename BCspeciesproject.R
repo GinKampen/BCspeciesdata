9b7d1fab7e2943cb9a5091764feab6db8610ff34
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-02 16:01:57 -0800

    add ggplot of distribution

--- BCspeciesproject.R
+++ BCspeciesproject.R
@@ -23,9 +23,25 @@ ecosection_map <- sf::st_read("data/ERC_ECOSECTIONS_SP/ERC_ECOSEC_polygon.shp")
 ### get codes for one specific species
 abies <- ecosections$Ecosection[ecosections$ScientificName == "Abies grandis / Berberis nervosa"]
 
-### get spatial data for one specific species - ecosection distribution
-abies_spatial <- ecosection_map[ecosection_map$ECOSEC_CD %in% abies,]
+library(rmapshaper)
 
+ecosection_simple <- ms_simplify(ecosection_map)
+
+### get spatial data for one specific species - ecosection distribution
+abies_spatial <- ecosection_simple[ecosection_simple$ECOSEC_CD %in% abies,]
+
+### alternatively add a column to ecosection data saing whether one species is present or not
+abies_present <- ecosection_simple
+abies_present$Present <- if_else(abies_present$ECOSEC_CD %in% abies, TRUE, FALSE)
+
+### colour by presence//absence
+library(ggplot2)
+
+gp <- ggplot() +
+  geom_sf(data = abies_present, aes(fill = Present), size = 0.1) +
+  scale_fill_manual(values = c("white", "red"))
+
+print(gp)
 ### NEXT STEP - how do we generalise this? 
 ### make a function where the user can provide a scientific name
 ### and the function returns spatial data ecosections for taht species 
