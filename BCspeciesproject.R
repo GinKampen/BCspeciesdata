d6614a8fee7dbd4331b287f2f0b8b8d73035964d
Gin Kampen <virginiakampen@gmail.com>
2020-03-09 14:53:11 -0700

    species map function ggplot

--- BCspeciesproject.R
+++ BCspeciesproject.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(sf)
 library(mapview)
 library(stringr)
+library(rmapshaper)
+library(ggplot2)
 
 species_bc <- readr::read_tsv(file = "data/bcsee_export.tsv")
 
@@ -19,35 +21,10 @@ ecosections <- do.call("rbind", lapply(1:nrow(ecosections), function(x){
 
 
 ecosection_map <- sf::st_read("data/ERC_ECOSECTIONS_SP/ERC_ECOSEC_polygon.shp")
-
-### get codes for one specific species - changed to include any species as input
-species <- "Alnus rubra / Carex obnupta [ Populus trichocarpa ]"
-species_ecosections <- ecosections$Ecosection[ecosections$ScientificName == species]
-
-
-library(rmapshaper)
+ecosection_map2 <- bcmaps::ecosections()
+bc_boundary <- bcmaps::bc_bound()
 
 ecosection_simple <- ms_simplify(ecosection_map)
-
-### get spatial data for one specific species - ecosection distribution - changed to include all species
-species_ecosections_spatial <- ecosection_simple[ecosection_simple$ECOSEC_CD %in% species_ecosections,]
-
-### alternatively add a column to ecosection data saying whether one species is present or not - changed for all species
-species_present <- ecosection_simple
-species_present$Present <- if_else(species_present$ECOSEC_CD %in% species_ecosections, TRUE, FALSE)
-
-### colour by presence//absence
-library(ggplot2)
-
-gp <- ggplot() +
-  geom_sf(data = species_present, aes(fill = Present), size = 0.1) +
-  scale_fill_manual(values = c("white", "red"))
-
-print(gp)
-
-### NEXT STEP - how do we generalise this? 
-### make a function where the user can provide a scientific name
-### and the function returns spatial data ecosections for taht species 
 
 ### Dropping columns
 species_bc$ScientificNameSynonyms <- NULL
@@ -63,6 +40,26 @@ species_bc$SARAComments <- NULL
 species_bc$BreedingBird <- NULL
 species_bc$MappingStatus <- NULL
 species_bc$X46 <- NULL
+
+species_map <- function(species) {
+  species_ecosections <- ecosections$Ecosection[ecosections$ScientificName == species]
+  species_ecosections_spatial <- ecosection_simple[ecosection_simple$ECOSEC_CD %in% species_ecosections,]
+  species_present <- ecosection_simple
+  species_present$Present <- if_else(species_present$ECOSEC_CD %in% species_ecosections, TRUE, FALSE)
+  gp <- ggplot() +
+    geom_sf(data = bc_boundary, size = 0.2) +
+    geom_sf(data = species_present, aes(fill = Present), size = 0.05) +
+    scale_fill_manual(values = c("transparent", "red"),
+                      name = " ",
+                      labels = c("Species Absent", "Species Present")) +
+    labs(title = "Species Distribution by Ecosection",
+         subtitle = species)
+                      return(print(gp))
+}
+
+
+species_map("Anemone occidentalis - Carex nigricans")
+
 
 
 
